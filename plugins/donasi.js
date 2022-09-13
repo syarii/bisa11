ae3c8f7eb2c76f153207b7d17ab11c41d8dfe61b
syarii <110132870+syarii@users.noreply.github.com>
2022-09-14 00:09:24 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, usedPrefix: _p, __dirname, args }) => {
 let text = `
 ┌─「 Donasi • Pulsa 」
-│ • *Telkomsel:* [${global.ppulsa}]
+│ • *Xl:* [${global.ppulsa}]
 ❏────
 
 ┌─「 Donasi • Non Pulsa 」
